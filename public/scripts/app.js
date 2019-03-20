b1251a1ae0946815d0713d2e248232c46535bac7
Estella Song <estellajaysong@gmail.com>
2019-03-20 23:44:20 +0000

    fixed submit function to display the new Tweet without refreshing the page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,29 +26,31 @@ function renderTweets(tweets) {
     $(".tweet-container").append(createTweetElement(tweet));
   });
 }
+// AJAX GET req to server for tweets and then render the Tweets
+function loadTweets() {
+  $.ajax("/tweets")
+    .done((response) => {
+      renderTweets(response)
+    })
+};
+loadTweets();
 
 // post-tweet POST req to server
-$( "#post-tweet" ).submit(function( event ) {
-  event.preventDefault();
-  if ($("textarea").val().length > 140) {
-    alert("Oops! Your Tweet has exceeded the 140-character limit")
-  } else if ($("textarea").val() === "") {
-    alert("Oops! You cannot submit an empty Tweet")
-  } else {
-  let data = ($(this).serialize());
-  $.post("/tweets", data);
-  };
-});
+  $("#post-tweet").submit(function (event) {
+    event.preventDefault();
+    if ($("textarea").val().length > 140) {
+      alert("Oops! Your Tweet has exceeded the 140-character limit");
+    } else if ($("textarea").val() === "") {
+      alert("Oops! You cannot submit an empty Tweet");
+    } else {
+      let data = ($(this).serialize());
+      $.post("/tweets", data, (response) => {
+          loadTweets();
+      })
+    };
+  });
 
-// AJAX GET req to server for tweets and then render the Tweets
- function loadTweets() {
-  $.ajax("/tweets")
-  .done((response) => {
-    renderTweets(response)
-  })
- };
 
-loadTweets();
 
 
 })
